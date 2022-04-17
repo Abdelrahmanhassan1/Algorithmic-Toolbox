712244b3beb6bb1618aaa99f9921ff5b0ee72f81
Abdelrahman Hassan <66575436+Abdelrahmanhassan1@users.noreply.github.com>
2022-04-17 16:59:34 +0200

    adding modified function

--- week 1/MaxPairwise_product.cpp
+++ week 1/MaxPairwise_product.cpp
@@ -15,6 +15,26 @@ long long naive_maxpair_product(const std::vector<int>& numbers){
     return max_product;
 }
 
+long long fast_maxpair_product(const std::vector<int>& numbers){
+    long long product = 0;
+    int max_index1 = -1;
+    int n = numbers.size();
+    for(int i = 0; i  < n ; i++){
+        if(numbers[i]> numbers[max_index1]){
+            max_index1 = i;
+        }
+    }
+    int max_index2 = -1;
+    for(int j = 0; j < n ; j ++){
+        if((numbers[j] > numbers[max_index2]) && (j != max_index1) ){
+            max_index2 = j;
+        }
+    }
+    product = (long long)numbers[max_index1] * numbers[max_index2];
+    return product; 
+    
+}
+
 
 
 int main(){
@@ -25,7 +45,34 @@ int main(){
     for(int i = 0; i< n; i++){
         std::cin >> numbers[i];
     }
-    std::cout<< naive_maxpair_product(numbers) << "\n";
+    // std::cout<< naive_maxpair_product(numbers) << "\n";
+    std::cout<< fast_maxpair_product(numbers) << "\n";
     return 0;
 
 }
+
+// // stress test 
+// int main(){
+//     while (true){
+//         int n;
+//         n = rand() % 1000 + 2;
+//         std::cout << n << "\n";
+//         std::vector<int>rand_numbers;
+//         for (int k = 0; k < n; k++){
+//             rand_numbers.push_back(rand()% 10000);
+//         }
+//         for (int k = 0; k < n; k++){
+//             std::cout << rand_numbers[k] << " ";
+//         }
+//         std::cout << "\n";
+//         long long result1 = naive_maxpair_product(rand_numbers);
+//         long long result2 = fast_maxpair_product(rand_numbers);
+//         if(result1 == result2){
+//             std::cout<<"OK \n";
+//         }else{
+//             std::cout<<"Wrong result:" << result1 << " " << result2 << "\n";
+//             break;
+//         }
+//     }
+//     return 0;
+// }
