27266dc766115d83df6844412ffbd705e6dcdc43
abdelrahmanhassan1 <abdelrahmanwork1@gmail.com>
2022-05-20 16:46:39 +0200

    adding week 2 code.

--- week_2/assigment2/fibonacci_sum_squares.cpp
+++ week_2/assigment2/fibonacci_sum_squares.cpp
@@ -18,8 +18,43 @@ int fibonacci_sum_squares_naive(long long n) {
     return sum % 10;
 }
 
+long long fibonacci_sum_squares_fast(long long to) {
+	if (to <= 1) return to;
+
+	bool flag = false;
+	long long previous = 0;
+	long long current = 1;
+	long long tmp_previous = 0;
+
+	long long seq_sum = 1;
+
+	for (long long i = 2; i < 60; ++i) {
+		tmp_previous = current;
+		current = (previous + current) % 10;
+		previous = tmp_previous;
+		seq_sum = (seq_sum + current * current) % 10;
+	}
+
+	seq_sum = (seq_sum * ((to / 60) % 10)) % 10;
+
+	previous = 0;
+	current = 1;
+	tmp_previous = 0;
+	for (size_t i = 0; i < 60; i++) {
+		if (i <= 1 and i <= to % 60)
+			seq_sum = (seq_sum + i) % 10;
+		else if (i <= to % 60) {
+			tmp_previous = current;
+			current = (current + previous) % 10;
+			previous = tmp_previous;
+			seq_sum = (seq_sum + current * current) % 10;
+		}
+	}
+	return seq_sum;
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_squares_naive(n);
+    std::cout << fibonacci_sum_squares_fast(n);
 }
